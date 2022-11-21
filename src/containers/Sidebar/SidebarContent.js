82cb6c9d5459623dcbb70706b960c8a283634152
Bruno Breña Ramírez <brcalix@gmail.com>
2022-11-21 02:05:45 -0500

    style: ui carga masiva

--- src/containers/Sidebar/SidebarContent.js
+++ src/containers/Sidebar/SidebarContent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu } from "antd";
+import { Button, Menu } from "antd";
 
 import CustomScrollbars from "util/CustomScrollbars";
 import SidebarLogo from "./SidebarLogo";
@@ -9,6 +9,8 @@ import {
 } from "../../constants/ThemeSetting";
 import { useSelector } from "react-redux";
 import { generateModules } from "../../util/generateModules";
+import { globalVariables } from "../../global.style";
+import { CloudUploadOutlined } from "@ant-design/icons";
 
 const SidebarContent = ({ sidebarCollapsed, setSidebarCollapsed }) => {
   const { navStyle, themeType } = useSelector(({ settings }) => settings);
@@ -41,6 +43,36 @@ const SidebarContent = ({ sidebarCollapsed, setSidebarCollapsed }) => {
       <div className="gx-sidebar-content">
         <div style={{ height: "20px" }} />
         <CustomScrollbars className="gx-layout-sider-scrollbar">
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: sidebarCollapsed ? "10px 15px" : "10px 40px",
+            }}
+          >
+            <Button
+              style={{
+                width: "100%",
+                backgroundColor: globalVariables.color.primary[100],
+                color: globalVariables.color.blanco,
+                border: "none",
+              }}
+              type="ghost"
+              // TODO: Mostrar pagina de carga masiva de folios
+              onClick={() => {}}
+            >
+              {sidebarCollapsed ? (
+                <CloudUploadOutlined
+                  style={{ fontSize: "18px", lineHeight: "2.5" }}
+                />
+              ) : (
+                "Importar archivo"
+              )}
+            </Button>
+          </div>
           <Menu
             defaultOpenKeys={[defaultOpenKeys]}
             selectedKeys={[selectedKeys]}
