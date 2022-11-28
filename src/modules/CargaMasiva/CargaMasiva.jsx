c589a9985552ad6e57d25cb1465ab28636ad81b8
Bruno Breña Ramírez <brcalix@gmail.com>
2022-11-27 22:42:10 -0500

    feat: insertMany folios

--- src/modules/CargaMasiva/CargaMasiva.jsx
+++ src/modules/CargaMasiva/CargaMasiva.jsx
@@ -1,19 +1,75 @@
-import { Form, Input, Modal } from "antd";
-import React from "react";
+import { Form, Image, Input, Modal } from "antd";
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+// * Styles
 import { globalVariables } from "../../global.style";
 import "./index.css";
+// * Assets
+import UploadFile from "../../assets/images/uploadFile.svg";
+// * Components
+import Boton from "../../components/Boton/Boton";
+// * Services
+import { foliosService } from "../../services/folios.service";
+import { openNotification } from "../../util/utils";
 
 const CargaMasiva = ({ verModal, setVerModal }) => {
   const [form] = Form.useForm();
+  const [loader, setLoader] = useState(false);
+  const [dataString, setDataString] = useState("");
+
+  const cargarArchivo = (ev) => {
+    setLoader(true);
+    let workBook;
+    let jsonData;
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+    console.log(reader);
+    console.log(file);
+    reader.onload = (event) => {
+      const data = reader.result;
+      console.log(data);
+      workBook = XLSX.read(data, { type: "binary" });
+      console.log(workBook);
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        console.log(sheet);
+        initial = { name: XLSX.utils.sheet_to_json(sheet) };
+        console.log(initial);
+        return initial;
+      }, {});
+      console.log(jsonData);
+      setDataString(JSON.stringify(jsonData));
+      setLoader(false);
+    };
+    reader.readAsBinaryString(file);
+    console.log(file);
+  };
+
+  const subirArchivo = async () => {
+    setLoader(true);
+    if (dataString === "") {
+      openNotification("Error", "No se ha cargado ningún archivo", "Alerta");
+      setLoader(false);
+      return;
+    }
+    const response = await foliosService.cargarFolios(dataString);
+    console.log(response);
+    if (response.data.statusCode === 200) {
+      // TODO: Abrir el componente para habilitar vehículos
+      // this.router.navigate(["./validar-folios"]);
+      setLoader(false);
+    }
+  };
 
   return (
     <Modal
       visible={verModal}
+      centered
       footer={false}
       onCancel={() => setVerModal(false)}
       maskClosable={false}
       maskStyle={{ backgroundColor: globalVariables.color.primary[100] }}
-      width={1000}
+      width={800}
     >
       <div
         style={{
@@ -22,13 +78,20 @@ const CargaMasiva = ({ verModal, setVerModal }) => {
           flexDirection: "row",
           alignItems: "center",
           justifyContent: "center",
+          gap: "20px",
         }}
       >
-        <div style={{ width: "30%" }}></div>
+        <div style={{ width: "40%" }}>
+          <Image
+            src={UploadFile}
+            alt="Upload File Image from UnDraw.co"
+            preview={false}
+          />
+        </div>
         <div
           style={{
-            width: "70%",
-            margin: "10px",
+            width: "60%",
+            margin: "20px",
             display: "flex",
             flexDirection: "column",
             alignItems: "start",
@@ -59,9 +122,24 @@ const CargaMasiva = ({ verModal, setVerModal }) => {
                 className="uploadfile-input"
                 type="file"
                 accept=".xls,.xlsx, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel"
+                onChange={(e) => cargarArchivo(e)}
               />
             </Form.Item>
           </Form>
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row-reverse",
+            }}
+          >
+            <Boton
+              type="primary"
+              onClick={subirArchivo}
+              name="Subir archivo"
+              loader={loader}
+            />
+          </div>
         </div>
       </div>
     </Modal>
